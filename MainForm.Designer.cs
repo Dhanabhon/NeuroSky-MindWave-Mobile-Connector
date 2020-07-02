f1a2c6937de1b0ea44e403f82f93344ea116e892
dhanabhon <thanaphon.spk@gmail.com>
2020-07-02 16:59:04 +0700

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace NeuroSkyMindWaveMobileConnector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectDisconnect = new System.Windows.Forms.Button();
+            this.lblAttentionLevel = new System.Windows.Forms.Label();
+            this.lblBandPower = new System.Windows.Forms.Label();
+            this.lblMeditationLevel = new System.Windows.Forms.Label();
+            this.lblAttentionLevelValue = new System.Windows.Forms.Label();
+            this.lblAlphaValue = new System.Windows.Forms.Label();
+            this.lblDeltaValue = new System.Windows.Forms.Label();
+            this.lblThetaValue = new System.Windows.Forms.Label();
+            this.lblBetaValue = new System.Windows.Forms.Label();
+            this.lblGammaValue = new System.Windows.Forms.Label();
+            this.lblMeditationLevelValue = new System.Windows.Forms.Label();
+            this.pbHeasetStatus = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeasetStatus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnectDisconnect
+            // 
+            this.btnConnectDisconnect.Location = new System.Drawing.Point(80, 30);
+            this.btnConnectDisconnect.Name = "btnConnectDisconnect";
+            this.btnConnectDisconnect.Size = new System.Drawing.Size(104, 35);
+            this.btnConnectDisconnect.TabIndex = 0;
+            this.btnConnectDisconnect.Text = "CONNECT";
+            this.btnConnectDisconnect.UseVisualStyleBackColor = true;
+            this.btnConnectDisconnect.Click += new System.EventHandler(this.btnConnectDisconnect_Click);
+            // 
+            // lblAttentionLevel
+            // 
+            this.lblAttentionLevel.AutoSize = true;
+            this.lblAttentionLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblAttentionLevel.Location = new System.Drawing.Point(12, 98);
+            this.lblAttentionLevel.Name = "lblAttentionLevel";
+            this.lblAttentionLevel.Size = new System.Drawing.Size(110, 16);
+            this.lblAttentionLevel.TabIndex = 1;
+            this.lblAttentionLevel.Text = "Attention Level";
+            // 
+            // lblBandPower
+            // 
+            this.lblBandPower.AutoSize = true;
+            this.lblBandPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblBandPower.Location = new System.Drawing.Point(12, 148);
+            this.lblBandPower.Name = "lblBandPower";
+            this.lblBandPower.Size = new System.Drawing.Size(91, 16);
+            this.lblBandPower.TabIndex = 2;
+            this.lblBandPower.Text = "Band Power";
+            // 
+            // lblMeditationLevel
+            // 
+            this.lblMeditationLevel.AutoSize = true;
+            this.lblMeditationLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblMeditationLevel.Location = new System.Drawing.Point(9, 304);
+            this.lblMeditationLevel.Name = "lblMeditationLevel";
+            this.lblMeditationLevel.Size = new System.Drawing.Size(122, 16);
+            this.lblMeditationLevel.TabIndex = 3;
+            this.lblMeditationLevel.Text = "Meditation Level";
+            // 
+            // lblAttentionLevelValue
+            // 
+            this.lblAttentionLevelValue.AutoSize = true;
+            this.lblAttentionLevelValue.Location = new System.Drawing.Point(12, 124);
+            this.lblAttentionLevelValue.Name = "lblAttentionLevelValue";
+            this.lblAttentionLevelValue.Size = new System.Drawing.Size(52, 13);
+            this.lblAttentionLevelValue.TabIndex = 4;
+            this.lblAttentionLevelValue.Text = "Attention:";
+            // 
+            // lblAlphaValue
+            // 
+            this.lblAlphaValue.AutoSize = true;
+            this.lblAlphaValue.Location = new System.Drawing.Point(12, 174);
+            this.lblAlphaValue.Name = "lblAlphaValue";
+            this.lblAlphaValue.Size = new System.Drawing.Size(37, 13);
+            this.lblAlphaValue.TabIndex = 5;
+            this.lblAlphaValue.Text = "Alpha:";
+            // 
+            // lblDeltaValue
+            // 
+            this.lblDeltaValue.AutoSize = true;
+            this.lblDeltaValue.Location = new System.Drawing.Point(9, 203);
+            this.lblDeltaValue.Name = "lblDeltaValue";
+            this.lblDeltaValue.Size = new System.Drawing.Size(35, 13);
+            this.lblDeltaValue.TabIndex = 6;
+            this.lblDeltaValue.Text = "Delta:";
+            // 
+            // lblThetaValue
+            // 
+            this.lblThetaValue.AutoSize = true;
+            this.lblThetaValue.Location = new System.Drawing.Point(9, 228);
+            this.lblThetaValue.Name = "lblThetaValue";
+            this.lblThetaValue.Size = new System.Drawing.Size(38, 13);
+            this.lblThetaValue.TabIndex = 7;
+            this.lblThetaValue.Text = "Theta:";
+            // 
+            // lblBetaValue
+            // 
+            this.lblBetaValue.AutoSize = true;
+            this.lblBetaValue.Location = new System.Drawing.Point(9, 254);
+            this.lblBetaValue.Name = "lblBetaValue";
+            this.lblBetaValue.Size = new System.Drawing.Size(32, 13);
+            this.lblBetaValue.TabIndex = 8;
+            this.lblBetaValue.Text = "Beta:";
+            // 
+            // lblGammaValue
+            // 
+            this.lblGammaValue.AutoSize = true;
+            this.lblGammaValue.Location = new System.Drawing.Point(9, 279);
+            this.lblGammaValue.Name = "lblGammaValue";
+            this.lblGammaValue.Size = new System.Drawing.Size(46, 13);
+            this.lblGammaValue.TabIndex = 9;
+            this.lblGammaValue.Text = "Gamma:";
+            // 
+            // lblMeditationLevelValue
+            // 
+            this.lblMeditationLevelValue.AutoSize = true;
+            this.lblMeditationLevelValue.Location = new System.Drawing.Point(9, 332);
+            this.lblMeditationLevelValue.Name = "lblMeditationLevelValue";
+            this.lblMeditationLevelValue.Size = new System.Drawing.Size(59, 13);
+            this.lblMeditationLevelValue.TabIndex = 10;
+            this.lblMeditationLevelValue.Text = "Meditation:";
+            // 
+            // pbHeasetStatus
+            // 
+            this.pbHeasetStatus.BackgroundImage = global::NeuroSkyMindWaveMobileConnector.Properties.Resources.NeuroSky_Signal_Disconnected;
+            this.pbHeasetStatus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbHeasetStatus.Location = new System.Drawing.Point(206, 12);
+            this.pbHeasetStatus.Name = "pbHeasetStatus";
+            this.pbHeasetStatus.Size = new System.Drawing.Size(70, 70);
+            this.pbHeasetStatus.TabIndex = 11;
+            this.pbHeasetStatus.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 362);
+            this.Controls.Add(this.pbHeasetStatus);
+            this.Controls.Add(this.lblMeditationLevelValue);
+            this.Controls.Add(this.lblGammaValue);
+            this.Controls.Add(this.lblBetaValue);
+            this.Controls.Add(this.lblThetaValue);
+            this.Controls.Add(this.lblDeltaValue);
+            this.Controls.Add(this.lblAlphaValue);
+            this.Controls.Add(this.lblAttentionLevelValue);
+            this.Controls.Add(this.lblMeditationLevel);
+            this.Controls.Add(this.lblBandPower);
+            this.Controls.Add(this.lblAttentionLevel);
+            this.Controls.Add(this.btnConnectDisconnect);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NeuroSky MindWave Mobile Connector";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeasetStatus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectDisconnect;
+        private System.Windows.Forms.Label lblAttentionLevel;
+        private System.Windows.Forms.Label lblBandPower;
+        private System.Windows.Forms.Label lblMeditationLevel;
+        private System.Windows.Forms.Label lblAttentionLevelValue;
+        private System.Windows.Forms.Label lblAlphaValue;
+        private System.Windows.Forms.Label lblDeltaValue;
+        private System.Windows.Forms.Label lblThetaValue;
+        private System.Windows.Forms.Label lblBetaValue;
+        private System.Windows.Forms.Label lblGammaValue;
+        private System.Windows.Forms.Label lblMeditationLevelValue;
+        private System.Windows.Forms.PictureBox pbHeasetStatus;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NeuroSky.ThinkGear;
+
+namespace NeuroSkyMindWaveMobileConnector
+{
+    public partial class MainForm : Form
+    {
+        private Connector neuroSkyConnector;
+        private volatile Device neursoSkyHeadset;
+
+        private delegate void AttentionLevelCallback(double attentionLevel);
+        private delegate void MeditationLevelCallback(double meditationLevel);
+        private delegate void PoorSignalCallback(int poorSignal);
+        //private delegate void AttentionLevelCallback(int attentionLevel);
+
+        private double deltaPower = 0.0;
+        private double thetaPower = 0.0;
+        private double alphaLowPower = 0.0;
+        private double alphaHighPower = 0.0;
+        private double betaLowPower = 0.0;
+        private double betaHighPower = 0.0;
+        private double gammaLowPower = 0.0;
+        private double gammaHighPower = 0.0;
+
+        private double attentionLevel = 0.0;
+        private double medtitationLevel = 0.0;
+        private double poorSignal = 200;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialize()
+        {
+            this.neuroSkyConnector = new Connector();
+            this.neuroSkyConnector.DeviceConnected += new EventHandler(this.OnDeviceConnected);
+            this.neuroSkyConnector.DeviceDisconnected += new EventHandler(this.OnDeviceDisconnected);
+            this.neuroSkyConnector.DeviceFound += new EventHandler(this.OnDeviceFound);
+            this.neuroSkyConnector.DeviceNotFound += new EventHandler(this.OnDeviceNotFound);
+            this.neuroSkyConnector.DeviceConnectFail += new EventHandler(this.OnDeviceConnectFail);
+            this.neuroSkyConnector.DeviceValidating += new EventHandler(this.OnDeviceValidating);
+
+            this.neuroSkyConnector.setBlinkDetectionEnabled(true);
+            this.neuroSkyConnector.StartEnergyLevel();
+            this.neuroSkyConnector.enableTaskDifficulty();
+            this.neuroSkyConnector.enableTaskFamiliarity();
+            this.neuroSkyConnector.getBlinkDetectionEnabled();
+        }
+
+        private void Connect()
+        {
+            this.neuroSkyConnector.ConnectScan();
+        }
+
+        private void Disconnect()
+        {
+            this.neuroSkyConnector.Disconnect();
+            this.neuroSkyConnector.Close();
+        }
+
+        private void UpdateAttentionLevel(double level)
+        {
+            if (this.lblAttentionLevel.InvokeRequired)
+            {
+                AttentionLevelCallback cb = new AttentionLevelCallback(this.UpdateAttentionLevel);
+                this.Invoke(cb, new object[] { level });
+            }
+            else
+            {
+                this.lblAttentionLevel.Text = level.ToString();
+            }
+        }
+
+        private void UpdateMeditationLevel(double level)
+        {
+            if (this.lblMeditationLevelValue.InvokeRequired)
+            {
+                MeditationLevelCallback cb = new MeditationLevelCallback(this.UpdateMeditationLevel);
+                this.Invoke(cb, new object[] { level });
+            }
+            else
+            {
+                this.lblMeditationLevelValue.Text = level.ToString();
+            }
+        }
+
+        private void UpdatePoorSignal(int poorSignal)
+        {
+            if (this.pbHeasetStatus.InvokeRequired)
+            {
+                PoorSignalCallback cb = new PoorSignalCallback(this.UpdatePoorSignal);
+                this.Invoke(cb, new object[] { poorSignal });
+            }
+            else
+            {
+                if (this.poorSignal < 200)
+                {
+                    this.pbHeasetStatus.BackgroundImage = Properties.Resources.NeuroSky_Signal_Fitting;
+                } 
+                else
+                {
+                    this.pbHeasetStatus.BackgroundImage = Properties.Resources.NeuroSky_Signal_Connected;
+                }
+            }
+        }
+
+        #region Events
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.Initialize();
+        }
+
+        private void btnConnectDisconnect_Click(object sender, EventArgs e)
+        {
+            if (this.btnConnectDisconnect.Text.Equals("CONNECT"))
+            {
+                this.btnConnectDisconnect.Text = "DISCONNECT";
+                this.Connect();
+            } else
+            {
+                this.btnConnectDisconnect.Text = "CONNECT";
+                this.Disconnect();
+            }
+        }
+
+        private void OnDeviceConnected(object sender, EventArgs e)
+        {
+            Connector.DeviceEventArgs deviceEventArgs = (Connector.DeviceEventArgs)e;
+            deviceEventArgs.Device.DataReceived += new EventHandler(this.OnDataReceived);
+        }
+
+        private void OnDataReceived(object sender, EventArgs e)
+        {
+            // Cast the event sender as a Device object, and e as the Device's DataEventArgs
+            this.neursoSkyHeadset = (Device)sender;
+            Device.DataEventArgs deviceDataEventArgs = (Device.DataEventArgs)e;
+
+            // Create a TGParser to parse the Device's DataRowArray[]
+            TGParser tgParser = new TGParser();
+
+            if (tgParser == null)
+            {
+                this.Disconnect();
+            }
+            else
+            {
+                tgParser.Read(deviceDataEventArgs.DataRowArray);
+
+                for (int i = 0; i < tgParser.ParsedData.Length; i++)
+                {
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerDelta"))
+                    {
+                        this.deltaPower = tgParser.ParsedData[i]["EegPowerDelta"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerTheta"))
+                    {
+                        this.thetaPower = tgParser.ParsedData[i]["EegPowerTheta"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerAlpha1"))
+                    {
+                        this.alphaLowPower = tgParser.ParsedData[i]["EegPowerAlpha1"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerAlpha2"))
+                    {
+                        this.alphaHighPower = tgParser.ParsedData[i]["EegPowerAlpha2"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerBeta1"))
+                    {
+                        this.betaLowPower = tgParser.ParsedData[i]["EegPowerBeta1"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerBeta2"))
+                    {
+                        this.betaHighPower = tgParser.ParsedData[i]["EegPowerBeta2"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerGamma1"))
+                    {
+                        this.gammaLowPower = tgParser.ParsedData[i]["EegPowerGamma1"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("EegPowerGamma2"))
+                    {
+                        this.gammaHighPower = tgParser.ParsedData[i]["EegPowerGamma2"];
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("Attention"))
+                    {
+                        this.attentionLevel = tgParser.ParsedData[i]["Attention"];
+                        this.UpdateAttentionLevel(this.attentionLevel);
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("Meditation"))
+                    {
+                        this.medtitationLevel = tgParser.ParsedData[i]["Meditation"];
+                        this.UpdateMeditationLevel(this.medtitationLevel);
+                    }
+                    if (tgParser.ParsedData[i].ContainsKey("PoorSignal"))
+                    {
+                        this.poorSignal = tgParser.ParsedData[i]["PoorSignal"];
+                        this.UpdatePoorSignal((int)this.poorSignal);
+                    }
+                }
+            }
+        }
+
+        private void OnDeviceDisconnected(object sender, EventArgs e)
+        {
+            this.pbHeasetStatus.Image = Properties.Resources.NeuroSky_Signal_Disconnected;
+        }
+
+        private void OnDeviceFound(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnDeviceNotFound(object sender, EventArgs e)
+        {
+            this.pbHeasetStatus.Image = Properties.Resources.NeuroSky_Signal_Disconnected;
+        }
+
+        private void OnDeviceConnectFail(object sender, EventArgs e)
+        {
+            this.pbHeasetStatus.Image = Properties.Resources.NeuroSky_Signal_Disconnected;
+        }
+
+        private void OnDeviceValidating(object sender, EventArgs e)
+        {
+
+        }
+        #endregion Events
+    } // class
+} // namespace
